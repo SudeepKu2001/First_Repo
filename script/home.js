238fe8d08dae174d58b2fe7762d03f9c111e7854
SudeepKu2001 <sudeepkumarparamguru53@gmail.com>
2024-10-07 19:25:20 +0530

    first commit

---
+++ script/home.js
@@ -0,0 +1,8 @@
+
+function toggleFunction(){
+    document.querySelector('.toggle-div').classList.toggle("active");
+}
+
+window.onscroll=()=>{
+   document.querySelector('.toggle-div').classList.remove("active");
+}
